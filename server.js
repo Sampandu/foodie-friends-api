123750d055ed9218067223a1f79c4807c9c0a864
Du <Sampan0103314@gmail.com>
2018-10-25 16:39:00 -0400

    delete the api key

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const apikey = '2u2d9VILWvjODb79fEZ4y1mXe8B4NfLAY9g2bIGEwMQdbN6ZtBZTScblejzr_vG0vgbtpEvB9IdsUHCPllt3edbnwov_RS4VCbPnF3HhxXa5pBnr4vAJ6Q5ZjDrLW3Yx';
+const apikey = '<your-api>';
 const client = yelp.client(apikey);
 
 app.get('/api/search', (req, res) => {
